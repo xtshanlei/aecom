b7f8d3441c374b02bac033c7297183cd5a0f392d
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-10-12 12:51:19 +0100

    Update aecom.py

--- aecom.py
+++ aecom.py
@@ -14,16 +14,15 @@ company_ls = ['AECOM','WSP Global Inc.','SNC-Lavalin Group Inc.','Webuild','Jaco
 aecom_stock = yf.Tickers(stock_code)
 
 st.subheader('Key Figures')
-@st.cache
-def extract_financial():
-    aecom_financial = aecom_stock.tickers['ACM'].financials
-    wsp_financial = aecom_stock.tickers['WSP.TO'].financials
-    snc_financial = aecom_stock.tickers['SNC.TO'].financials
-    wbd_financial = aecom_stock.tickers['WBD.MI'].financials
-    jacob_financial = aecom_stock.tickers['J'].financials
-    flr_financial = aecom_stock.tickers['FLR'].financials
-    eme_financial = aecom_stock.tickers['EME'].financials
-    mtz_financial = aecom_stock.tickers['MTZ'].financials
+
+aecom_financial = aecom_stock.tickers['ACM'].financials
+wsp_financial = aecom_stock.tickers['WSP.TO'].financials
+snc_financial = aecom_stock.tickers['SNC.TO'].financials
+wbd_financial = aecom_stock.tickers['WBD.MI'].financials
+jacob_financial = aecom_stock.tickers['J'].financials
+flr_financial = aecom_stock.tickers['FLR'].financials
+eme_financial = aecom_stock.tickers['EME'].financials
+mtz_financial = aecom_stock.tickers['MTZ'].financials
 st.dataframe(aecom_financial)
 
 financial_df = pd.DataFrame()
