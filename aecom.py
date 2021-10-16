17342eeb013af07b55ce63f42c69de755e5de605
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-10-16 22:48:13 +0100

    Update aecom.py

--- aecom.py
+++ aecom.py
@@ -42,6 +42,7 @@ def get_financial():
     eme_financial = aecom_stock.tickers['EME'].financials
     mtz_financial = aecom_stock.tickers['MTZ'].financials
 get_financial()
+st.write(aecom_financial)
 def get_financial_item(item):
     financial_df = pd.DataFrame()
     financial_df['Company'] = company_ls
