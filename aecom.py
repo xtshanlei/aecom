65b1efe053bb27a4a11be63536120ea3fe89027a
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-10-16 19:56:09 +0100

    Update aecom.py

--- aecom.py
+++ aecom.py
@@ -129,7 +129,7 @@ import streamlit.components.v1 as components
 st.subheader('TOP 10 Topics Discussed on Twitter')
 top_topics=pd.read_csv('top_10_topics.csv')
 st.dataframe(top_topics)
-top_topics_fig = px.bar(topic_topics,x='Count', y='Name',orientation='h')
+top_topics_fig = px.bar(top_topics,x='Count', y='Name',orientation='h')
 st.plotly_chart(top_topics_fig)
 
 HtmlFile = open("dynamic_topic.html", 'r', encoding='utf-8')
