56c0f3171d604608482fda0d20bd056476b40b96
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-10-22 10:59:48 +0100

    Update aecom.py

--- aecom.py
+++ aecom.py
@@ -59,7 +59,7 @@ def get_financial():
     return aecom_financial,wsp_financial,snc_financial,wbd_financial,jacob_financial,flr_financial,eme_financial,mtz_financial
 with st.spinner('Extracting latest financial information, please wait...'):
     aecom_financial,wsp_financial,snc_financial,wbd_financial,jacob_financial,flr_financial,eme_financial,mtz_financial = get_financial()
-st.success('Extraction completed!')
+st.success('Financial information extracted!')
 
 
 
